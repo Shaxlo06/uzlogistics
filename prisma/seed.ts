a8ad463ad0177d50a8acf2329c6f6dfdc741e20a
Shaxlo06 <shaxlosobirovna06@gmail.com>
2026-08-13 05:43:14 +0500

    Fix Prisma seed client import

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../app/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { PrismaLibSql } from "@prisma/adapter-libsql";
 import bcrypt from "bcryptjs";
 import seedData from "../data/companies_seed.json";
